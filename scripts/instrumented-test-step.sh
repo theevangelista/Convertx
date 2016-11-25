4161472c24182554a381b0c22524e3edd89a5b76
João Pedro Evangelista <joaoevangelista@protonmail.com>
2016-11-25 18:49:24 -0200

    Fix bash variable parsing?

--- scripts/instrumented-test-step.sh
+++ scripts/instrumented-test-step.sh
@@ -1,8 +1,10 @@
+AVD="$1"
+
 echo 'Preparing for Instrumented Tests...'
-echo 'Starting AVD $1'
-./scripts/run-avd.sh $1
+echo "Starting AVD ${AVD}"
+./scripts/run-avd.sh "${AVD}"
 echo 'Executing Instrumented Tests...'
 ./gradlew deviceAndroidTest # Installs and runs instrumentation tests using all Device Providers.
 # Optionaly you can use
 # connectedAndroidTest - Installs and runs instrumentation tests for all flavors on connected devices.
-echo '--- DONE executing Instrumented Tests for $1 ---'
+echo "--- DONE executing Instrumented Tests for ${AVD} ---"
