509376b1a8505257cacc88050d644d204089d645
João Pedro Evangelista <joaoevangelista@protonmail.com>
2016-11-25 20:31:07 -0200

    Fix build image?

--- scripts/run-avd.sh
+++ scripts/run-avd.sh
@@ -1,4 +1,4 @@
-echo no | android create avd --force -n test -t android-25 --abi armeabi-v7a --tag google_apis
+echo no | android create avd --force -n test -t android-24 --abi armeabi-v7a
 emulator -avd test -no-audio -no-window &
 android-wait-for-emulator
 adb shell input keyevent 82 &
