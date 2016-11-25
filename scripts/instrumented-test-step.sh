11dbad4dfa6a9e1ef80de51718f563bc39a4ebcf
João Pedro Evangelista <joaoevangelista@protonmail.com>
2016-11-25 20:13:42 -0200

    Fix build image?

--- scripts/instrumented-test-step.sh
+++ scripts/instrumented-test-step.sh
@@ -1,10 +1,8 @@
-AVD="$1"
-
 echo 'Preparing for Instrumented Tests...'
-echo "Starting AVD ${AVD}"
-./scripts/run-avd.sh "${AVD}"
+echo "Starting AVD"
+./scripts/run-avd.sh
 echo 'Executing Instrumented Tests...'
 ./gradlew deviceAndroidTest # Installs and runs instrumentation tests using all Device Providers.
 # Optionaly you can use
 # connectedAndroidTest - Installs and runs instrumentation tests for all flavors on connected devices.
-echo "--- DONE executing Instrumented Tests for ${AVD} ---"
+echo "--- DONE executing Instrumented Tests ---"
--- scripts/run-avd.sh
+++ scripts/run-avd.sh
@@ -1,5 +1,4 @@
-android list sdk --extended --no-ui --all | grep abi
-echo no | android create avd --force -n test -t "$1" --abi sys-img-armeabi-v7a-android-"$1"
+echo no | android create avd --force -n test -t android-25 --abi armeabi-v7a --tag google_apis
 emulator -avd test -no-audio -no-window &
 android-wait-for-emulator
 adb shell input keyevent 82 &
