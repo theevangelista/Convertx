0577ea6dd211bbc714de08cb23fc75f99eab937a
João Pedro Evangelista <joaoevangelista@protonmail.com>
2016-11-25 19:20:02 -0200

    Change abi name on run-avd script

--- scripts/run-avd.sh
+++ scripts/run-avd.sh
@@ -1,4 +1,4 @@
-echo no | android create avd --force -n test -t "$1" --abi armeabi-v7a-"$1"
+echo no | android create avd --force -n test -t "$1" --abi sys-img-armeabi-v7a-android-"$1"
 emulator -avd test -no-audio -no-window &
 android-wait-for-emulator
 adb shell input keyevent 82 &
