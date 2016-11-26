705878352e2ea3cf17ba3319796a7787ae7419fc
João Pedro Evangelista <joaoevangelista@protonmail.com>
2016-11-25 22:25:55 -0200

    Change instrumentation task

--- scripts/instrumented-test-step.sh
+++ scripts/instrumented-test-step.sh
@@ -2,7 +2,5 @@ echo 'Preparing for Instrumented Tests...'
 echo "Starting AVD"
 ./scripts/run-avd.sh
 echo 'Executing Instrumented Tests...'
-./gradlew deviceAndroidTest # Installs and runs instrumentation tests using all Device Providers.
-# Optionaly you can use
-# connectedAndroidTest - Installs and runs instrumentation tests for all flavors on connected devices.
+./gradlew connectedAndroidTest
 echo "--- DONE executing Instrumented Tests ---"
